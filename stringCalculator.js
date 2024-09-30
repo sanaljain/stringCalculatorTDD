7f7e9c3946da87d538e1d157abfd59d830ee036a
SHRANYA <sanal1997@gmail.com>
2024-09-30 17:24:59 +0530

    checking for numbers seperated by custom delimiter

--- stringCalculator.js
+++ stringCalculator.js
@@ -14,8 +14,8 @@ function add(numbers) {
   return tempNumbers.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
-// console.log(add(""), "Output should be 0");
-// console.log(add("1"), "output should be 1");
-// console.log(add("1,2,3"), "Output should be 6");
-// console.log(add("1\n2,3"), "Output should be 6");
-console.log(add("//;\n1;2;3"));
+console.log(add(""), "Output should be 0");
+console.log(add("1"), "output should be 1");
+console.log(add("1,2,3"), "Output should be 6");
+console.log(add("1\n2,3"), "Output should be 6");
+console.log(add("//;\n1;2", "Output should be 3"));
