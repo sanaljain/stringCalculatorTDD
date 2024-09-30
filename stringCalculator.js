2fe3fbfa6ff067d947e29eda8c93d81287c3427f
SHRANYA <sanal1997@gmail.com>
2024-09-30 17:40:46 +0530

    greater than 1000 discarded

--- stringCalculator.js
+++ stringCalculator.js
@@ -14,12 +14,16 @@ function add(numbers) {
       `negative numbers not allowed: ${negativeNumbers.join(", ")}`
     );
   }
-  return tempNumbers.reduce((sum, num) => sum + parseInt(num), 0);
+  return tempNumbers.reduce((sum, num) => {
+    let temp = parseInt(num);
+    return temp > 1000 ? sum : sum + temp;
+  }, 0);
 }
 
 console.log(add(""), "Output should be 0");
 console.log(add("1"), "output should be 1");
 console.log(add("1,2,3"), "Output should be 6");
 console.log(add("1\n2,3"), "Output should be 6");
-console.log(add("//;\n1;2", "Output should be 3"));
-console.log(add("1,2,3,-1,-2,4,5,-5"), "Error");
+console.log(add("//;\n1;2"), "Output should be 3");
+// console.log(add("1,2,3,-1,-2,4,5,-5"), "Error");
+console.log(add("2,1001,5"), "greater than 1000 discarded");
