2ad09fe74caa2649004541f822f3498aa2d78bd2
SHRANYA <sanal1997@gmail.com>
2024-09-30 17:12:02 +0530

    checking for numbers seperated by comma and a new line

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,9 +1,10 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  let tempNumbers = numbers.split(",");
+  let tempNumbers = numbers.split(/[\n,]/);
   return tempNumbers.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
 console.log(add(""), "Output should be 0");
 console.log(add("1"), "output should be 1");
 console.log(add("1,2,3"), "Output should be 6");
+console.log(add("1\n2,3"), "Output should be 6");
