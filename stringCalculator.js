c8d541a6283dc1347c64cdc53c79434b620f2d91
SHRANYA <sanal1997@gmail.com>
2024-09-30 17:24:33 +0530

    checking for numbers seperated by custom delimiter

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,10 +1,21 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  let tempNumbers = numbers.split(/[\n,]/);
+
+  let delimiter = /[\n,]/;
+  if (numbers.startsWith("//")) {
+    let delimiterPart = numbers.match(/\/\/(.+)\n/);
+    console.log(delimiterPart);
+    delimiter = new RegExp(delimiterPart[1]);
+    console.log(delimiter);
+    numbers = numbers.split("\n")[1];
+    console.log(numbers);
+  }
+  let tempNumbers = numbers.split(delimiter);
   return tempNumbers.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
-console.log(add(""), "Output should be 0");
-console.log(add("1"), "output should be 1");
-console.log(add("1,2,3"), "Output should be 6");
-console.log(add("1\n2,3"), "Output should be 6");
+// console.log(add(""), "Output should be 0");
+// console.log(add("1"), "output should be 1");
+// console.log(add("1,2,3"), "Output should be 6");
+// console.log(add("1\n2,3"), "Output should be 6");
+console.log(add("//;\n1;2;3"));
