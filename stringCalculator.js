6d891a2332223667c50d22083020eebd32494d62
SHRANYA <sanal1997@gmail.com>
2024-09-30 17:33:14 +0530

    checking with negetive numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -3,14 +3,17 @@ function add(numbers) {
 
   let delimiter = /[\n,]/;
   if (numbers.startsWith("//")) {
-    let delimiterPart = numbers.match(/\/\/(.+)\n/);
-    console.log(delimiterPart);
-    delimiter = new RegExp(delimiterPart[1]);
-    console.log(delimiter);
+    let tempDelimiter = numbers.match(/\/\/(.+)\n/);
+    delimiter = new RegExp(tempDelimiter[1]);
     numbers = numbers.split("\n")[1];
-    console.log(numbers);
   }
   let tempNumbers = numbers.split(delimiter);
+  let negativeNumbers = tempNumbers.filter((num) => parseInt(num) < 0);
+  if (negativeNumbers.length > 0) {
+    throw new Error(
+      `negative numbers not allowed: ${negativeNumbers.join(", ")}`
+    );
+  }
   return tempNumbers.reduce((sum, num) => sum + parseInt(num), 0);
 }
 
@@ -19,3 +22,4 @@ console.log(add("1"), "output should be 1");
 console.log(add("1,2,3"), "Output should be 6");
 console.log(add("1\n2,3"), "Output should be 6");
 console.log(add("//;\n1;2", "Output should be 3"));
+console.log(add("1,2,3,-1,-2,4,5,-5"), "Output should be 6");
