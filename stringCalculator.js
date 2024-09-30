d8da8218dfdb11abf87d94f767366fbb71bfc80d
SHRANYA <sanal1997@gmail.com>
2024-09-30 18:07:31 +0530

    multiple delimeters

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,11 +1,23 @@
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
 function add(numbers) {
   if (numbers === "") return 0;
 
   let delimiter = /[\n,]/;
   if (numbers.startsWith("//")) {
-    let tempDelimiter = numbers.match(/\/\/(.+)\n/);
-    delimiter = new RegExp(tempDelimiter[1]);
-    numbers = numbers.split("\n")[1];
+    let singleTempDelimiter = numbers.match(/\/\/(.)\n/);
+    let tempDelimiter = numbers.match(/\/\/\[(.+?)\]\n/);
+    if (singleTempDelimiter) {
+      delimiter = new RegExp(escapeRegExp(singleTempDelimiter[1]));
+      numbers = numbers.split("\n")[1];
+    } else if (tempDelimiter) {
+      let custom = tempDelimiter[1];
+      custom = escapeRegExp(custom);
+      delimiter = new RegExp(custom);
+      numbers = numbers.split("\n")[1];
+    }
   }
   let tempNumbers = numbers.split(delimiter);
   let negativeNumbers = tempNumbers.filter((num) => parseInt(num) < 0);
@@ -24,6 +36,7 @@ console.log(add(""), "Output should be 0");
 console.log(add("1"), "output should be 1");
 console.log(add("1,2,3"), "Output should be 6");
 console.log(add("1\n2,3"), "Output should be 6");
-console.log(add("//;\n1;2"), "Output should be 3");
+console.log(add("//;\n1;2"), "Custom delimiterwith single delimiter");
 // console.log(add("1,2,3,-1,-2,4,5,-5"), "Error");
 console.log(add("2,1001,5"), "greater than 1000 discarded");
+console.log(add("//[***]\n1***2***3"), "Custom multiple delimiter");
